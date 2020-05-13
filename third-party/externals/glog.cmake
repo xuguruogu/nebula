bd1fe3f38b761f7d1a3780addd3abf65e8c16ca3
trippli <trippli@tencent.com>
2020-05-13 15:53:21 +0800

    glog extention modify

--- third-party/externals/glog.cmake
+++ third-party/externals/glog.cmake
@@ -15,6 +15,7 @@ ExternalProject_Add(
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
     SOURCE_DIR ${source_dir}
+    PATCH_COMMAND patch -p0 < ${CMAKE_SOURCE_DIR}/patches/glog-0.3.5.patch
     CONFIGURE_COMMAND
         ${common_configure_envs}
         ./configure ${common_configure_args}
