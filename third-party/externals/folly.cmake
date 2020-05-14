6ce276b8fa1241745f41e951ea183994d5e82131
trippli <trippli@tencent.com>
2020-05-14 12:48:24 +0800

    patch folly

--- third-party/externals/folly.cmake
+++ third-party/externals/folly.cmake
@@ -15,6 +15,7 @@ ExternalProject_Add(
     STAMP_DIR ${BUILD_INFO_DIR}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
     SOURCE_DIR ${source_dir}
+    PATCH_COMMAND patch -p0 < ${CMAKE_SOURCE_DIR}/patches/folly-2018-08-20.patch
     CMAKE_ARGS
         ${common_cmake_args}
         -DCMAKE_BUILD_TYPE=Release
