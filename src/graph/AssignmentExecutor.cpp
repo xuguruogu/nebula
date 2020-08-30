361ea2a897a49e8e57fa4340baeaa8f08c91c6cd
trippli <trippli@tencent.com>
2020-08-30 16:22:08 +0800

    sentence not access data need not permission check.

--- src/graph/AssignmentExecutor.cpp
+++ src/graph/AssignmentExecutor.cpp
@@ -50,11 +50,6 @@ Status AssignmentExecutor::prepare() {
 
 
 void AssignmentExecutor::execute() {
-    auto status = checkIfGraphSpaceChosen();
-    if (!status.ok()) {
-        doError(std::move(status));
-        return;
-    }
     executor_->execute();
 }
 
